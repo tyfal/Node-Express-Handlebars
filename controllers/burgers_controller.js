a3b240587f701f83c970df8d711cfd213316d13d
tyfal <tfalcoff@gmail.com>
2019-09-18 18:19:20 -0500

    updated model, controller and views

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require(`express`);
+
+var router = express.Router();
+
+var burger = require(`../models/burger`);
+
+router.get(`/`, (req, res) => {
+    burger.all((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+    });
+    res.render(`index`, hbsObject);
+});
+
+router.post(`/api/burgers`, (req, res) => {
+    burger.create([
+        `burger_name`,
+        `devoured`
+    ], [
+        req.body.name,
+        req.body.devoured
+    ], (result) => {
+        res.json({ id: result.insertId});
+    });
+});
+
+router.put(`/api/burgers/:id`, (req, res) => {
+    var condition = `id = ${req.params.id}`;
+
+    console.log(`condition: ${condition}`);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
+
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require(`../config/orm`);
+
+var burger = {
+    all: (cb) => {
+        orm.selectAll(`burgers`, (res) => {
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.insertOne(`burgers`, cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.updateOne(`burgers`, objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
